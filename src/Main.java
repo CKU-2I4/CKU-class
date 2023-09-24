8d5708d2a5f8f3a65fe7d5e53dc88ec15bf9736b
PrzemekTryc <przemek.tryc@gmail.com>
2023-09-24 13:01:42 +0200

    Zad1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-
+zad1();
     }
 
-    private void zad1() {
+    private static void zad1() {
+        System.out.println("Podaj liczbe: ");
+        Scanner scanner = new Scanner(System.in);
+        int liczba = scanner.nextInt();
+
+        if (liczba > 0) {
+            System.out.println("Liczba jest dodatnia.");
+
+        } else if (liczba < 0) {
+            System.out.println("Liczba jest ujemna.");
+
+        } else {
+            System.out.println("Liczba jest rowna zero.");
+        }
 
     }
-
 
 }
+
+
+
+
+
+
+
