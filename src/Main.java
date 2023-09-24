73685338a8a42998f05c848a0e17b937df362c27
Andrzej <sloniua@gmail.com>
2023-09-24 12:36:32 +0200

    rozwiazanie zad1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-
-
+        zad1();
     }
 
-    private void zad1() {
-        if()
+    private static void zad1() {
+        int liczba = -4;
+
+        if (liczba > 0) {
+            System.out.println("Podana liczba jest dodatnia.");
+        } else if (liczba < 0) {
+            System.out.println("Podana liczba jest ujemna.");
+        } else {
+            System.out.println("Podana liczba jest równa zeru.");
+        }
+
     }
 
 
