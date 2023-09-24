b4a780728e21340c95153a297a3cadc5a675ca88
Andrzej <sloniua@gmail.com>
2023-09-24 12:43:07 +0200

    rozwiazanie zad2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        zad1();
+//        zad1();
+        zad2();
     }
 
     private static void zad1() {
@@ -16,5 +17,13 @@ public class Main {
 
     }
 
+    private static void zad2() {
+
+        for (int i = 1; i < 21; i++) {
+            System.out.println(i);
+        }
+    }
+
 
 }
+
