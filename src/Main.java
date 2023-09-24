b677a4f651a997115135739c85670e724797628f
Andrzej <sloniua@gmail.com>
2023-09-24 13:32:58 +0200

    rozwiazanie zad3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 //        zad1();
-        zad2();
+//        zad2();
+        zad3();
     }
 
     private static void zad1() {
@@ -22,6 +26,39 @@ public class Main {
         for (int i = 1; i < 21; i++) {
             System.out.println(i);
         }
+    }
+
+    private static void zad3() {
+
+        Random random = new Random();
+        int liczba = random.nextInt(100);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("podaj liczbe");
+
+        int myNum = scanner.nextInt();
+
+//        if (liczba == myNum) {
+//            System.out.println("Bravo");
+//        } else if (liczba > myNum) {
+//            System.out.println("wylosowana liczba jest wieksza");
+//        } else {
+//            System.out.println("wylosowana liczba jest mniejsza");
+//        }
+
+
+        while (liczba != myNum) {
+            if (liczba > myNum) {
+                System.out.println("wylosowana liczba jest wieksza");
+            } else {
+                System.out.println("wylosowana liczba jest mniejsza");
+            }
+            myNum = scanner.nextInt();
+
+        }
+
+        System.out.println("bravo zgadłeś/aś");
     }
 
 
